170b8b0601c07af3c1a23388082e3a192008b89e
carlos.fortun@bitsamericas.com <carlos.fortun@bitsamericas.com>
2020-12-08 21:58:36 -0500

    Prueba basica exitosa

--- tests/Util/FizzBuzzTest.php
+++ tests/Util/FizzBuzzTest.php
@@ -8,7 +8,7 @@ class FizzBuzzTest extends TestCase
 {
     public function testFizzBuzz(){
         $data = range(1,100);
-        $result = $data[0] % 3;
+        $result = $data[2] % 3;
         $this->assertEquals(0,$result);
     }
 
