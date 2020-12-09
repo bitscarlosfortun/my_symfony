1fb62b221160faf1cb9f50a95da88dfa5acabc7d
carlos.fortun@bitsamericas.com <carlos.fortun@bitsamericas.com>
2020-12-08 22:25:50 -0500

    3 CU exitosos

--- tests/Util/FizzBuzzTest.php
+++ tests/Util/FizzBuzzTest.php
@@ -6,10 +6,27 @@ use PHPUnit\Framework\TestCase;
 
 class FizzBuzzTest extends TestCase
 {
-    public function testFizzBuzz(){
-        $data = range(1,100);
-        $result = $data[2] % 3;
+	protected function setUp(){
+		$this->data = range(1,100);
+	}
+	
+	protected function setDown(){
+		unset($this->data);
+	}
+	
+	public function testFizz(){        
+        $result = $this->data[2] % 3;
         $this->assertEquals(0,$result);
+    }
+	
+	public function testBuzz(){        
+        $result = $this->data[4] % 5;
+        $this->assertEquals(0,$result);
+    }
+	
+    public function testFizzBuzz(){        
+        $result = $this->data[14] % 3 == 0 && $this->data[14] % 5 == 0;
+        $this->assertEquals(true,$result);
     }
 
 }
